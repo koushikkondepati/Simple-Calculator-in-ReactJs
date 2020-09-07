2d84feba68b00c4998afe15d738a7d2103529769
koushikkondepati <koushikkondepati4255@gmail.com>
2020-09-07 18:24:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {add,sub,Mul,Div} from './Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+    return(
+        <>
+            <div>
+                <h1>Basic Calculator</h1>
+                <ol>
+                    <li>Sum of the two numbers is : {add(30,20)}</li>
+                    <li>Sub of the two numbers is : {sub(30,20)}</li>
+                    <li>Mul of the two numbers is : {Mul(30,20)}</li>
+                    <li>Div of the two numbers is : {Div(30,20)}</li>
+                </ol>
+            </div>
+        </>
+    )
+};
 
 export default App;
---
+++ src/Calculator.jsx
@@ -0,0 +1,21 @@
+function add(a,b){
+    let c = a+b;
+    return c;
+}
+
+function sub(a,b){
+    let c = a-b;
+    return c;
+}
+
+function Mul(a,b){
+    let c = a*b;
+    return c;
+}
+
+function Div(a,b){
+    let c = a/b;
+    return c;
+}
+
+export {add,sub,Mul,Div};
